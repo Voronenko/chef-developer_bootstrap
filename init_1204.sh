1eaf70556a55ad093751d107231c9d83e4d00337
Vyacheslav <voronenko@gmail.com>
2014-05-25 23:09:19 +0300

    Upgraded init file for boxes based on 12.04 (set to ruby 2.1)

---
+++ init_1204.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#recommended for 12.04
+
+sudo apt-get install python-software-properties
+
+sudo apt-add-repository ppa:brightbox/ruby-ng
+
+sudo apt-get update
+
+sudo apt-get install ruby2.1 rubygems ruby-switch
+
+sudo ruby-switch --set ruby1.9.1
+
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+#librarian
+echo installing librarian gem... Please be patient, takes a while.
+sudo gem install librarian-chef --no-ri --no-rdoc
+
+#conflicts with berk. No berk installed for manual box conf.
+
+echo Initializing cookbooks
+librarian-chef install
