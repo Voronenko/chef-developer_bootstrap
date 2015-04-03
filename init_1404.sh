1df048ae6cb9985deee74f07121be99a98f95738
Vyacheslav Voronenko <git@voronenko.com>
2015-04-03 21:59:33 +0300

    Changes caused by Nginx and MongoDB updates

---
+++ init_1404.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#recommended for 14.04 LTS
+
+sudo add-apt-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+
+sudo apt-get install ruby2.1 ruby2.1-dev build-essential libssl-dev zlib1g-dev curl
+
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+#librarian
+echo installing librarian gem... Please be patient, takes a while.
+sudo gem install librarian-chef --no-ri --no-rdoc
+
+#conflicts with berk. No berk installed for manual box conf.
+
+echo Initializing cookbooks
+librarian-chef install
